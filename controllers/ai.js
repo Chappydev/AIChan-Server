15b1836db2e0dc6eb1f2b95e22d71354d6ab044e
ChappyDev <danjtchapman@gmail.com>
2024-05-07 15:48:13 -0600

    Port over custom route from AIChan

--- controllers/ai.js
+++ controllers/ai.js
@@ -68,7 +68,34 @@ aiRouter.post("/particles", async (req, res) => {
   }
 });
 
-aiRouter.get("/", (req, res) => {
+aiRouter.post("/custom", async (req, res) => {
+  // try {
+  //   await applyRateLimiting(req, res);
+  // } catch {
+  //   return res.status(429).send("Request Limit Exceeded");
+  // }
+  if (!(req.body && req.body.content)) {
+    return res.status(400).json({ error: "must include content" });
+  }
+  const { content } = req.body;
+  console.log(content);
+
+  try {
+    const data = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: content,
+      temperature: 0.5,
+    });
+    console.log(data);
+
+    res.status(200).json(data.choices[data.choices.length - 1].message);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+
+aiRouter.get("/", (_, res) => {
   res.json({
     whathappened: "it worked",
   });
